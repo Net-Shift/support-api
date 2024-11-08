b9580d28314cfafab5ab6f10ed0d13e525f47b3e
Guillaume Leger <guillaumeleger140@gmail.com>
2024-11-08 23:53:59 +0100

    update exceptions handler

--- app/exceptions/handler.ts
+++ app/exceptions/handler.ts
@@ -1,5 +1,8 @@
 import app from '@adonisjs/core/services/app'
 import { HttpContext, ExceptionHandler } from '@adonisjs/core/http'
+import { errors } from '@adonisjs/core'
+import { errors as AuthErrors } from '@adonisjs/auth'
+import { errors as VineErrors } from '@vinejs/vine'
 
 export default class HttpExceptionHandler extends ExceptionHandler {
   /**
@@ -13,6 +16,38 @@ export default class HttpExceptionHandler extends ExceptionHandler {
    * response to the client
    */
   async handle(error: unknown, ctx: HttpContext) {
+
+    if (error instanceof errors.E_ROUTE_NOT_FOUND) {
+      return ctx.response.status(error.status).send({
+        success: false,
+        message: 'Route not found',
+      })
+    }
+  
+    if (error instanceof AuthErrors.E_UNAUTHORIZED_ACCESS) {
+      return ctx.response.status(error.status).send({
+        success: false,
+        message: 'Unauthorized access',
+      })
+    }
+  
+    if (error instanceof VineErrors.E_VALIDATION_ERROR) {
+      console.log('error', error)
+      return ctx.response.status(error.status).send({
+        success: false,
+        message: 'Validation error',
+        data: error.messages,
+      })
+    }
+
+    const customError = error as any
+    if (customError.code === 'E_ROW_NOT_FOUND') {
+      return ctx.response.status(customError.status).send({
+        success: false,
+        message: 'Data row not found',
+      })
+    }
+
     return super.handle(error, ctx)
   }
 
