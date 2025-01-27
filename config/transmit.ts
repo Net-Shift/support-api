9bdd069b16bb23257ef25ccc754d41c25a55a297
Guillaume Leger <guillaumeleger140@gmail.com>
2025-01-27 23:43:32 +0100

    fix transmit

--- config/transmit.ts
+++ config/transmit.ts
@@ -1,13 +1,17 @@
 import { defineConfig } from '@adonisjs/transmit'
 import { redis } from '@adonisjs/transmit/transports'
+import env from '#start/env'
+
+const redisUrl = env.get('REDIS_URL')
+const redisParsedUrl = new URL(redisUrl)
 
 export default defineConfig({
   pingInterval: false,
   transport: {
     driver: redis({
-      host: '127.0.0.1',
-      port: 6379,
-      password: '',
+      host: redisParsedUrl.hostname,
+      port: parseInt(redisParsedUrl.port, 10),
+      password: redisParsedUrl.password || '',
       keyPrefix: 'transmit',
     })
   }
