d383cef5990cc2d02aae1ef6633e617062c24bba
Guillaume Leger <guillaumeleger140@gmail.com>
2025-01-27 23:13:57 +0100

    test

--- start/env.ts
+++ start/env.ts
@@ -20,6 +20,7 @@ export default await Env.create(new URL('../', import.meta.url), {
   APP_URL: Env.schema.string.optional(),
   API_URL: Env.schema.string(),
   PASSWORD_RESET_PAGE_URL: Env.schema.string(),
+  
 
   /*
   |----------------------------------------------------------
