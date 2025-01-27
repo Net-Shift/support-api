7ba68c64852261385e0b50af00ce984d49d4edbc
Guillaume Leger <guillaumeleger140@gmail.com>
2025-01-27 23:06:59 +0100

    fix env

--- start/env.ts
+++ start/env.ts
@@ -18,7 +18,7 @@ export default await Env.create(new URL('../', import.meta.url), {
   HOST: Env.schema.string.optional({ format: 'host' }),
   LOG_LEVEL: Env.schema.enum.optional(['fatal', 'error', 'warn', 'info', 'debug', 'trace']),
   APP_URL: Env.schema.string.optional(),
-  // API_URL: Env.schema.string(),
+  API_URL: Env.schema.string(),
   PASSWORD_RESET_PAGE_URL: Env.schema.string(),
 
   /*
